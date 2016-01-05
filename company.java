abd55a1cbe82d86e0cff38d7ceb9d02bac0d32da
Idan <idan.hovav16@gmail.com>
2016-01-05 15:22:53 -0800

    Capitalized letter, added description

---
+++ company.java
@@ -0,0 +1,17 @@
+/*
+Companies have a stock value that is based on their company value divided
+by the number of stocks they have.
+Companies can pay dividends.
+Company value can go up or down randomly (sin).
+
+Variables x,y,z used for describing sin functions.
+
+*/
+
+
+public class Company{
+	String name;
+	int stock;
+	float val;
+	float x,y,z;
+}
---
+++ stockexchange.java
@@ -0,0 +1,15 @@
+public class stockexchange{
+	public static void main(String[] args){
+		//args has name
+		
+		/*
+		Gameplan: stock exchange has the main method, a setup method.
+		Setup sets up the game with a player instance, company instances, and 
+		starting round 0.
+		Main method plays a round in a while loop. Buying/selling, 
+		updating stock values, quit, pay dividends. Takes in user input.
+
+
+		*/
+	}
+}
